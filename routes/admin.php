032e7a10519438dbd148a6b3359aebd694ac3bc2
axy320 <arismaulana06445@gmail.com>
2026-09-01 15:36:55 +0800

    fix halaman admin

--- routes/admin.php
+++ routes/admin.php
@@ -6,5 +6,7 @@ use Inertia\Inertia;
 
 Route::middleware(['auth', 'staff'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', fn () => Inertia::render('Admin/Dashboard'))->name('dashboard');
-    Route::resource('products', ProductController::class);
+    Route::resource('products', ProductController::class)
+        ->except(['show'])
+        ->names('products');
 });
